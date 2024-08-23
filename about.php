ca6f5847ac3230528b9c0d085d93ae611b33f284
PriyaBhadekar <priyabhadekar05@gmail.com>
2024-08-23 13:58:52 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,283 @@
+<?php
+include("header.php");
+?>
+<div id="home-owl" class="owl-carousel owl-theme">
+
+<div class="home-item">
+
+<div class="section-bg" style="background-image: url(img/downloadfile1.jpg);"></div>
+
+
+<div class="home">
+<div class="container">
+<div class="row">
+<div class="col-md-8">
+<div class="home-content">
+<h1>FeedingHope</h1>
+<p class="lead">From Waste to Well-being: Join FeedingHope..</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+
+<div class="home-item">
+
+<div class="section-bg" style="background-image: url(img/IMG_20191210_124518.jpg);"></div>
+
+
+<div class="home">
+<div class="container">
+<div class="row">
+<div class="col-md-8">
+<div class="home-content">
+<h1>FeedingHope</h1>
+<p class="lead">FeedingHope, we believe in the power of community-driven solutions to tackle the interconnected challenges of food waste and hunger. Our platform serves as a catalyst for change, connecting surplus food providers with individuals and organizations in need..</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</header>
+
+
+<div id="about" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-5">
+<div class="section-title">
+<h2 class="title">Welcome to FeedingHope</h2>
+<p class="sub-title">At FeedingHope, we believe in the power of community-driven solutions to tackle the interconnected challenges of food waste and hunger. Our platform serves as a catalyst for change, connecting surplus food providers with individuals and organizations in need.</p>
+
+</div>
+</div>
+
+
+<div class="col-md-offset-1 col-md-6">
+<a href="#" class="about-video">
+<img src="img/charity/IMG_20191210_133502.jpg" style="heighT: 500px;">
+</a>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<div id="numbers" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-smile-o"></i>
+<h3>47k</h3>
+<span>Donors</span>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-heartbeat"></i>
+<h3>154K</h3>
+<span>Food Donated</span>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-money"></i>
+<h3>785K</h3>
+<span>Donated</span>
+</div>
+</div>
+
+
+<div class="col-md-3 col-sm-6">
+<div class="number">
+<i class="fa fa-handshake-o"></i>
+<h3>357</h3>
+<span>Volunteers</span>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<hr>
+<div id="causes" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-12">
+<div class="section-title">
+<b>First Unit -  FeedingHope</b>
+<p class="sub-title">Every day, tons of perfectly edible food are discarded by businesses, events, and households, contributing to both environmental degradation and the perpetuation of food insecurity. FeedingHope steps in to intercept this surplus food before it goes to waste, redirecting it to those who need it most.</p>
+<p class="sub-title">Through our user-friendly website, donors can easily list their excess food items, whether it's leftover from events, restaurants, or personal gatherings. Our network of partner organizations, including shelters, food banks, and community centers, then efficiently coordinate the pickup and distribution of these donations to ensure they reach individuals and families facing hunger.</p>
+<p class="sub-title">But our mission doesn't stop there. FeedingHope is committed to fostering sustainable change by addressing the root causes of food waste and hunger. In addition to facilitating food donations, we also empower communities through educational initiatives, advocacy efforts, and innovative projects aimed at reducing food waste and promoting food security.</p>
+<p class="sub-title">Our fundraising section plays a crucial role in sustaining our operations and expanding our impact. By donating to FeedingHope, supporters contribute to our ongoing efforts to build a more equitable and resilient food system, where no one goes to bed hungry and where surplus food becomes a valuable resource for those in need.</p>
+<br>
+<b>Second Unit : Abhaya Bala Mandira</b>
+<p class="sub-title"> Started in the year 1980-81,  Abhaya Bala Mandira is accepting Destitute/Orphan boys in the age group of 5 to 18 years. Now the strength is 28, of which two are studying engineering. The academic performance of students is very much satisfactory and quite impressive. Students appeared for SSLC and PUC have passed mostly with 1st class and 2nd class. They attend nearby school and colleges. On school reopening day all our children are lead in a procession after offering of “Arathi”, “Kumkum” and sweets, blessed by our president Sri. Shreenath Hegde,  office bearers and elderly residents..</p><br>
+
+
+<b>Third Unit:-  Dr. Giridhar Rao-Sanjivi Bai Vriddhashrama “Vathsalyadham” Kodialbail  Bangalore.</b>
+<p class="sub-title">"Vathsalyadham" was opened on 15-08-1986. Late Dr. Radha, leading Gynacologist our biggest benevolent donor (birth: 1-12-1925 and death: 11.05.1985) who bequeathed the entire premises with an endowment of Rs. 11.75 Lakhs under instructions to utilize the proceeds for the maintenance of destitute and elderly residents. We have kept up the spirit of the will to the best of our ability, which now is a Shelter for more than fifty, residents.</p><br>
+
+
+<b>SERVICE.</b>
+<p class="sub-title">Full-fledged physiotherapy units donated by Rural Electrification Corporation Limited, New Delhi through “HelpAge” India are also functioning  at Sahaya Charity Assaigoli and at “Vathsalyadham”, Bangalore. We are also provided with ambulance donated by HelpAge India. Happy to state that Sahaya Charity is recognized by the by the State Govt. Of Karnataka..</p>
+<p class="sub-title">Our residents belong to the poorest of the poor families without the economic, cultural or educational background of the privileged, without access even to the basic necessities of life like shelter, food, healthcare and education. Having lived a life of extreme poverty and hardships, they are depressed  without any enthusiasm in life. As such all our activities are centered on changing their attitude towards life and motivating them to dignified.</p>
+<p class="sub-title">living with awareness programme of social security and benefits to elders by the Government. Free eye camps, medical camps, dental camps are regularly conducted by Sahaya Charity at backward remote villages, in association with various social service organisations and through other Medical and Dental Colleges. Rehabilitation of widows, destitutes and  needy are given priority. Living quarters are kept clean with regular maintenance. Sahaya Charity with both units is  considered as the one of the best managed organisation, known for its personal care and medical facilities..</p>
+
+<p class="sub-title"> Regret to report that since inception, 642 residents left their mortal frames, but happy to state that it was not on roads or drains but under roof of love and care, true to our motto supra.
+</p>
+
+<p class="sub-title">We appeal to the privileged sections of the society
+                             to please extend their helping hand.
+</p>
+
+
+
+</div>
+</div>
+
+
+</div>
+
+</div>
+
+</div>
+
+
+<div id="cta" class="section">
+
+<div class="section-bg" style="background-image: url(img/background-1.jpg);" data-stellar-background-ratio="0.5"></div>
+
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-offset-2 col-md-8">
+<div class="cta-content text-center">
+<h1>Join with Us</h1>
+<p class="lead">Join us in our mission to turn surplus into support, to share hope, and to nourish communities. Together, we can make a tangible difference in the lives of individuals and families, one meal at a time.
+..</p>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<div id="events" class="section">
+
+<div class="container">
+
+<div class="row">
+
+<div class="col-md-8 col-md-offset-2">
+<div class="section-title text-center">
+<h2>FeedingHope</h2>
+</div>
+</div>
+
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<a href="single-event.html">
+<img src="img/event-1.jpg" alt="">
+</a>
+</div>
+<div class="event-content">
+<h3><a href="single-event.html">Help people in need</a></h3>
+<p>Provide direct support to an individual, family or community by paying medical expenses or offering financial aid..</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<a href="single-event.html">
+<img src="img/event-2.jpg" alt="">
+</a>
+</div>
+<div class="event-content">
+<h3><a href="single-event.html">Take action in an emergency</a></h3>
+
+<p>Raise funds in response to a natural disaster or humanitarian crisis. Make a difference in minutes.</p>
+</div>
+</div>
+</div>
+
+<div class="clearfix visible-md visible-lg"></div>
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<a href="single-event.html">
+<img src="img/event-3.jpg" alt="">
+</a>
+</div>
+<div class="event-content">
+<h3><a href="single-event.html">Take part in a charity event</a></h3>
+
+<p>Choose from hundreds of official events including marathons, bike rides, Dryathlons and bake offs….</p>
+</div>
+</div>
+</div>
+
+
+<div class="col-md-6">
+<div class="event">
+<div class="event-img">
+<a href="single-event.html">
+<img src="img/event-4.jpg" alt="">
+</a>
+</div>
+<div class="event-content">
+<h3><a href="single-event.html">Celebrate an occasion</a></h3>
+<p>Mark a special event like a birthday, wedding or final exam by asking friends for donations rather than gifts..</p>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+<?php
+include("footer.php");
+?>
